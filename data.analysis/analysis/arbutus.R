f17a50dd76e26b07f40a44999a24dfb7512692f0
Heath Blackmon <coleoguy@gmail.com>
2015-06-02 12:05:08 -0500

    work on chrome.num paper

--- data.analysis/analysis/arbutus.R
+++ data.analysis/analysis/arbutus.R
@@ -4,7 +4,7 @@
 ## Heath Blackmon
 ## 28 April 2015
 ## coleoguy@gmail.com
-
+library(arbutus)
 load("../results/genera.bm")
 taxa   <- c('Bembidion', 'Calathus', 'Chrysolina', 
             'Cicindela', 'Cytronus', 'Dendroctonus', 
@@ -12,12 +12,23 @@ taxa   <- c('Bembidion', 'Calathus', 'Chrysolina',
             'Pterostichus', 'Timarcha')
 
 
-plot(arbutus(results[[12]][[1]]))
-
+arb.results <- list()
 # now lets loop through and combine 
 # all the arbutus results across trees
 for(i in 1:12){
+  cat(paste("\nworking on", taxa[i]))
+  group.results <- list()
   for(j in 1:100){
-    
+    group.results[[j]] <- arbutus(results[[i]][[j]], nsim = 10)
+    cat(".")
   }
+  arb.results[[i]] <- group.results
 }
+
+# ok so now we extract the arbutus results from all trees in a group 
+# and combine so we have a null and observed distribution
+
+
+
+# look at geiger 
+
